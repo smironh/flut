d63b5ad4f175ebd39cfd6cb4ea44bd7c98e18693
smiron <71022332+smironh@users.noreply.github.com>
2021-01-07 21:15:49 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# flut
-spymer on python
+# установка
+<code>apt update</code><br>
+• <code>apt upgrade</code><br>
+• <code>apt install git</code><br>
+• <code>git clone https://github.com/smironh/flut.git</code><br>
+• <code>sh ~/flut/install.sh</code><br>
+• <code>flut</code><br>
+
+# также и на linux
---
+++ install.sh
@@ -0,0 +1,31 @@
+clear
+echo "введите куда устанавливаете"
+echo "1:unix"
+echo "2:termux"
+if {$numb}
+    then
+        pkg install python
+        pkg install dos2unix
+        pip install requests colorama proxyscrape
+        cp ~/bomb/setup.py $PREFIX/bin/flut
+        dos2unix $PREFIX/bin/flut
+        chmod -R 777 ~/flut
+        chmod 777 $PREFIX/bin/flut
+        flut
+
+        else
+	if [ $numb = "2" ]
+	then
+		if [ "$(whoami)" != 'root' ];
+		then
+			echo "У вас нет прав. Запустите install.sh с root правами (sudo sh ~/flut/install.sh)"
+			exit
+		else
+			apt install python3 python3-pip dos2unix
+			pip3 install requests colorama proxyscrape
+			cp ~/flut/spammer.py $PREFIX/bin/flut
+			dos2unix $RPEFIX/bin/flut
+			chmod 777 $RPEFIX/bin/flut
+			chmod -R 777 ~/flut
+			flut
+		fi
